028dc51b354e7c5bd09b7058dc560525a610eab7
Santiago Palacio <spalac24@localhost.localdomain>
2013-08-25 23:03:13 -0500

    shortened hw3_2.py

--- python/hw3_2.py
+++ python/hw3_2.py
@@ -10,14 +10,9 @@ def factor(n, d = 2):
     return factor(n,d+1)
 
 n = int(sys.argv[1])
-if (n > 1000000):
-    print "El numero es mayor que 1e6"
-    sys.exit()
-
-elif (n <= 0):
-    print "El numero es menor o igual a 0"
-    sys.exit()
-
+if (not (0 < n <= 1000000)):
+    print "El numero no se encuentra en el intervalo"
+    
 f = factor(n)
 if (len(f) == 2):
     print "%d %d"%(f[0],f[1])
