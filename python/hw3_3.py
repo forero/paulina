c9fb09bc53d1ed46f3c6aec1ff1d1422f9d13e74
Santiago Palacio <spalac24@localhost.localdomain>
2013-08-26 01:47:01 -0500

    addig beautiful extended slice syntax to hw3_3

--- python/hw3_3.py
+++ python/hw3_3.py
@@ -1,16 +1,11 @@
 import string
 import sys
 
-def reverse (s):
-    l = list(s)
-    l.reverse()
-    return ''.join(l)
-
 def remove_not_letters (s):
     return ''.join([a for a in s if a.isalpha()])
 
 s =  remove_not_letters(''.join(open(sys.argv[1]).readlines())).upper()
-if (reverse(s) == s):
+if (s[::-1] == s):
     print "Es un palindromo"
 else:
     print "No es un palindromo"
