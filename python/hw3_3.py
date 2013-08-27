0e619f779861966fcdd4460a69f220fe28ffa63a
Santiago Palacio <spalac24@gmail.com>
2013-08-27 14:48:43 -0500

    changed hw3_3.py to support spanish characters

--- python/hw3_3.py
+++ python/hw3_3.py
@@ -1,10 +1,12 @@
+# -*- coding: utf-8 -*- 
+
 import string
 import sys
 
 def remove_not_letters (s):
     return ''.join([a for a in s if a.isalpha()])
 
-s =  remove_not_letters(''.join(open(sys.argv[1]).readlines())).upper()
+s =  remove_not_letters(''.join(open(sys.argv[1]).readlines()).decode('utf-8')).upper()
 if (s[::-1] == s):
     print "Es un palindromo"
 else:
